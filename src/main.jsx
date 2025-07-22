b4d313859e1c3da1e0f89d3171f0cd505bf20e0f
Abhishek-jii <143919591+Abhishek-jii@users.noreply.github.com>
2025-07-22 18:38:56 +0530

    server url added

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
 // export const server = "https://college-back.onrender.com";
-export const server = "http://localhost:5000";
+export const server = "https://e-learning-server-ngki.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
